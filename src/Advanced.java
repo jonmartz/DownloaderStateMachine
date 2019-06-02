3c1e0c87fdb111520ff75028b2fc2cace60d95a3
guy schlesinger <guys797@gmail.com>
2019-06-02 16:24:38 +0300

    minor design change + professional fix

--- src/Advanced.java
+++ src/Advanced.java
@@ -5,6 +5,11 @@ public class Advanced extends UserStatus {
         super();
         Context.getInstance().setOnCurrentState(Enum.OnRegionNames.MANEGERING_USER_STATUS,this);
         Context.getInstance().speed = 1.2;
+
+    }
+
+    @Override
+    public void entry() {
         if (Context.getInstance().points>6){
             Context.getInstance().changeStateIfOn(Enum.OnRegionNames.MANEGERING_USER_STATUS,Enum.StateNames.PROFESSIONAL);
         }
